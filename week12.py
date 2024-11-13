67d25eeb00c4fc96fdd140c7b7a92af2ef6a39f0
inhadeepblue <deepblue@inha.ac.kr>
2024-11-13 11:26:43 +0900

    v0.7 prime number

--- week12.py
+++ week12.py
@@ -1,12 +1,13 @@
 is_prime = True
 number = int(input("Input number : "))
 
-if number <= 1:  # bug fix
+if number <= 1:
     is_prime = False
 else:
     for i in range(2, number):
         if number % i == 0:
             is_prime = False
+            break  # performance up
         print(i, end=' ')
 
 if is_prime:
