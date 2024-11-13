fa763f688cb6d809ad52452cf2b7f4a223bcdf74
inhadeepblue <deepblue@inha.ac.kr>
2024-11-13 10:39:26 +0900

    v0.3 prime number

--- week12.py
+++ week12.py
@@ -1,7 +1,7 @@
 counts = 0
 number = int(input("Input number : "))
 
-for i in range(1, number):
+for i in range(2, number):
     if number % i == 0:
         counts = counts + 1
     print(i, end=' ')
