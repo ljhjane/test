363fe407bcff5f4c2141e53ced90a74affdc2854
inhadeepblue <deepblue@inha.ac.kr>
2024-11-13 10:52:15 +0900

    v0.5 prime number

--- week12.py
+++ week12.py
@@ -1,12 +1,15 @@
-counts = 0
+#counts = 0
+is_prime = True  # int -> bool
 number = int(input("Input number : "))
 
 for i in range(2, number):
     if number % i == 0:
-        counts = counts + 1
+        #counts = counts + 1  # remove +
+        is_prime = False
     print(i, end=' ')
 
-if counts == 0:  # bug fix
+# if counts == 0:
+if is_prime:  # remove ==
     print(f"\n{number} is prime number~")
 else:
     print(f"\n{number} is NOT prime number! (divisors : {counts})")
